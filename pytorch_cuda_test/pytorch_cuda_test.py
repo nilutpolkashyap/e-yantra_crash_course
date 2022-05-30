179e615b362469a1a2e0befb94e1f6556143da32
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2022-05-31 00:01:44 +0530

    code for pytorch cuda test
    python code to check if pytorch is built with nvidia cuda

---
+++ pytorch_cuda_test/pytorch_cuda_test.py
@@ -0,0 +1,5 @@
+import torch 
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+# pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+
